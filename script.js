feedcfc25cca8c4f96845b4bf680e618fa642f65
Ahmed Mohamed <ahmed.mohamed57@outlook.com>
2022-10-31 00:59:08 -0700

    Boeing

---
+++ script.js
@@ -0,0 +1,19 @@
+let max737 
+let button2
+
+function welcome() {
+  alert("Warning! The following might give you a new phobia.")
+  let data = String(prompt("Have you heard about the 737 max?"));
+  console.log(data)
+  return data;
+
+}
+
+console.log(welcome())
+
+
+
+max737 = document.getElementById("max737").addEventListener("click", function() {
+  alert("Boeing 737 MAX was announced in 2011 in competition with Norwegian jets. 3000 planes sold to American and foreign airlines. But it wasn't manufactured until 2015. First patch of planes were delieverd to airlines in late 2017.")
+})
+
