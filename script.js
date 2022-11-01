3b6bd2b5f8bd21a70bf8e14cf5685078422d4d3f
Ahmed Mohamed <ahmed.mohamed57@outlook.com>
2022-10-31 21:06:20 -0700

    fixed function

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
-<<<<<<< HEAD
 let button
-=======
-let max737 
->>>>>>> c6a89ce197ed786e942974422d0156ffa3ca8c8a
 
 function welcome() {
   alert("Warning! The following might give you a new phobia.")
   let data = String(prompt("Have you heard about the 737 max?"));
-  console.log(data)
-  return data;
+  getName();
+  getPrompt()
+
+  button = document.getElementById("max737").addEventListener("click", function () {
+    alert("Boeing 737 MAX was announced in 2011 in competition with Norwegian jets. 3000 planes sold to American and foreign airlines. But it wasn't manufactured until 2015. First patch of planes were delieverd to airlines in late 2017.")
+  })
+}
+
+
+function getName() {
+  let userName = prompt("What is your name");
+  if (userName == "" || userName == null || userName == undefined) {
+    alert("try Again");
+    getName();
+
+  } else {
+    document.write("Hey " + userName + ", Welcome to my Page")
+  }
+}
+
+function getPrompt() {
+
+  let answer = "2018"
+  response = prompt("What year did the 737 MAX crash in Indonesia? ")
+
+  while (response !== answer) {
+      alert("Incorrect Try again") 
+    getPrompt()
+  }
 
 }
 
-console.log(welcome())
-
-function getName (){
-    let userName = prompt("What is your name");
-    if (userName == "" || userName == null || userName == undefined){
-        alert("try Again");
-        getName();
-
-    } else {
-        document.write("Hey " + userName + ", Welcome to my Page")
-    }
-}
-
-getName();
-
-
-
-button = document.getElementById("max737").addEventListener("click", function() {
-  alert("Boeing 737 MAX was announced in 2011 in competition with Norwegian jets. 3000 planes sold to American and foreign airlines. But it wasn't manufactured until 2015. First patch of planes were delieverd to airlines in late 2017.")
-})
-
+welcome();
