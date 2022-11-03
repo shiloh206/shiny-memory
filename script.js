240a27e0b71d62ed0ff571f283d660a54edaa403
Ahmed Mohamed <ahmed.mohamed57@outlook.com>
2022-11-02 17:21:57 -0700

    added img.

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ function welcome() {
   alert("Warning! The following might give you a new phobia.")
   let data = String(prompt("Have you heard about the 737 max?"));
   getName();
-  getPrompt()
+  getPrompt();
+  planes()
 
   button = document.getElementById("max737").addEventListener("click", function () {
     alert("Boeing 737 MAX was announced in 2011 in competition with Norwegian jets. 3000 planes sold to American and foreign airlines. But it wasn't manufactured until 2015. First patch of planes were delieverd to airlines in late 2017.")
@@ -36,3 +37,14 @@ function getPrompt() {
 }
 
 welcome();
+
+function planes(){
+  let planes = prompt("How many planes do you want?)");
+  while(planes < 1 || planes > 100){
+      planes = prompt("Bad entry. (1-100 planes)")
+  }
+  for (let i = 0; i < planes; i++) {
+      document.write("<img src='plane.jpg'></img>")
+      document.write(i+1)
+  }
+}
